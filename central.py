e87306c51e6ee35f40d17ca20a2d2d254498e07c
tgymartin <tgymartin@users.noreply.github.com>
2017-03-16 18:10:14 +0800

    Add files via upload

---
+++ central.py
@@ -0,0 +1,41 @@
+from firebase import firebase
+
+#database
+url = "https://rasppi2-efc3c.firebaseio.com/" # URL to Firebase database
+token = "96ph75e8wFRh1pS2IjkowbVP9onHN5YZl6g23jnN" # unique token used for authentication
+
+firebase = firebase.FirebaseApplication(url, token)
+
+class account(object):
+    pass
+
+class washMachine(object):
+    def __init__(self, id):
+        self.id = id
+        self.lockstate = False
+    def wash(self):
+        #directly manipulate database
+        print 'id: %d \nWashing...' %(self.id)
+    def lock(self):
+        #directly manipulate database
+        print 'id: %d \nLocking...' %(self.id)
+    def unlock(self):
+        #directly manipulate database
+        print 'id: %d \nUnlocking' %(self.id)
+        pass
+
+def UI():
+    pass
+
+def main():
+    account = {}
+    machine = {}
+
+    
+    
+
+    
+main()
+
+def updateData(name, info):
+    pass # update the database
