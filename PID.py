3ea9b96dd32e4dffeacc41b982bf952d7a926de1
Martin <tgymartin@gmail.com>
2017-04-06 16:16:07 +0800

    updated PID.py

--- PID.py
+++ PID.py
@@ -7,6 +7,7 @@ import sys
 
 class PID(object): #needs import time
     def __init__(self, Kp = 1, Ki = 0, Kd = 0, min_output = 0, max_output = 1, integrator_min = -sys.maxint, integrator_max = sys.maxint,  derivativeN = 5, backCalc = False, backCalcCoeff = 0.0):
+        print 'linwei is a nutter'
         self.Kp = Kp #Proportional constant
         self.Ki = Ki #Integral Constant
         self.Kd = Kd #Derivative Constant
@@ -16,7 +17,7 @@ class PID(object): #needs import time
         self.integrator_max = integrator_max
         self.min_output = min_output
         self.max_output = max_output
-        self.prevError = None
+        self.prevError = 0
         self.prevTime = time.time()
         self.clamped = False
         
@@ -28,10 +29,12 @@ class PID(object): #needs import time
         self.backCalc = backCalc
         self.backCalcCoeff = backCalcCoeff
         self.prevOut = None
+        self.backCalcLevel = 0.0
+        self.setpoint = 0.0
 
     def __call__(self, x):
         #calculate dt = time since previous output measurement
-        timeNow = time.clock() #get the time once to prevent waiting for execution time
+        timeNow = time.time() #get the time once to prevent waiting for execution time
         dt = timeNow - self.prevTime
         self.prevTime = timeNow #save current time as previous time for next reading
         
@@ -39,21 +42,25 @@ class PID(object): #needs import time
         error = self.setpoint - x
         self.P_val = self.Kp * error
         
-        #clamp the I value
+        # clamp the I value
         if self.clamped == False:
-            if self.I_val < self.integrator_max and self.I_val > self.integrator_min:
-                self.I_val += self.Ki * error * dt
+            print 'integrating'
+            self.I_val += self.Ki * error * dt
 
-        #reduce the integral using back calculation if signal is clamp
+            if self.I_val > self.integrator_max:
+                self.I_val = self.integrator_max
+            if self.I_val < self.integrator_min:
+                self.I_val = self.integrator_min
+
+        # reduce the integral using back calculation if signal is clamped
         if self.backCalc == True and self.clamped == True:
             self.I_val += self.Ki * (error - (self.backCalcCoeff * self.backCalcLevel)) * dt
         
-        #Calculate the moving average of the derivatives to reduce sensor noise.
-        if self.prevError != None:
-            self.derivVals[self.derivIdx] = (error - self.prevError) / dt
-        self.D_val = self.Kd * reduce(lambda x, y: x + y, self.derivVals)/float(self.derivN)
+        self.derivVals[self.derivIdx] = (error - self.prevError) / dt
+        self.D_val = self.Kd * sum(self.derivVals)/float(self.derivN)
+        print error - self.prevError
         self.derivIdx = (self.derivIdx + 1) % self.derivN #advance the index of the list
-        error = self.prevError
+        self.prevError = error
 
         output = self.I_val + self.P_val + self.D_val
         self.prevOut = output
@@ -69,10 +76,11 @@ class PID(object): #needs import time
             self.backCalcLevel = output - self.prevOut
         else:
             self.clamped = False
-
+        print 'P = %.3f, I = %.6f, D = %.5f, dt = %.3f' %(self.P_val, self.I_val, self.D_val, dt)
         return output
 
     def set_setpoint(self, setpoint):
+        print 'setpoint = %.3f' %(setpoint)
         self.setpoint = setpoint
 
 class movingAverage(object):
