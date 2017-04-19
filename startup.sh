79dd8ae99e544e041675e9e4551c5e069531dcf3
Martin Tan <marteenee@Martins-MacBook-Pro.local>
2017-04-19 18:02:46 +0800

    updated startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #reference this file in /etc/rc.local
 
-sudo apt-get update
+sudo apt-get update -y
 cd #location of folder
+git pull
+# rm -r DW1D
+# git clone https://github.com/Zhi-en/DW1D.git
+# cd DW1D
 python #files to run
